c06aacb74bc2b291a8d95db1ab7c13dee02b0b5c
affvini <vinicius.f.veras@gmail.com>
2022-11-10 08:04:06 -0300

    Novas aulas

---
+++ Arrays.js
@@ -0,0 +1,8 @@
+
+const alunos = ['João', 'Maria', 'Jose'];
+
+alunos.push('Renan')
+
+alunos[4] = 'Vinicius'
+
+console.log(alunos[0]);
---
+++ tabuada.js
@@ -0,0 +1,13 @@
+const tabuada = 3
+
+let num = 1
+
+console.log(`A tabuada de ${tabuada} é: `);   
+
+for (let i = 1; i <= 10; i++) {    
+    const valor = i * tabuada;    
+    console.log(`${tabuada} X ${i} = ${valor}`);
+
+    
+}
+
